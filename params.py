049cf2aa997127f8088060bb8d09f8dd227bffb8
nils <nils>
2024-12-09 12:12:51 +0100

    update params

--- params.py
+++ params.py
@@ -1,13 +1,62 @@
 import json
 
 from functools import reduce, partial
+from itertools import repeat
 import operator
 
 import numpy as np
 
+"""
+See main below for simple example of basic usage.
+
+```
+params = [
+    Parameter(...),
+    Parameter(...),
+    Parameter(...),
+]
+
+for ctr, settings in enumerate(pproduct(params)):
+    p = SimpleNamespace(**settings)
+    ...
+```
+
+- Use `Parameter` to create basic 1 dimensional parameter iterators. 
+- Use `Parameter2D` to create a 2 dimenional grid.
+- Use `pproduct` to build a cartesian product out of the iteratorss. Note
+  that it resets iterators for each "nested loop" to make sure new randoms
+  are generated whenever a new iteration begins.
+- Use `ptotal` to return the total amount of combinations to be returned
+  from a list of `Parameter`s.
+- Use `pdump` and `pload` to dump to and load from a json file.
+"""
+
 class Parameter():
+    """
+    `Parameter` supports the following `itype`s (iterator types):
+
+    - `fixed`: Always returns the same value (i.e. `min`), useful to collect extra information for posterity.
+
+    - `repeat`: Repeat `min` `count` times.
+        
+        `[x, x, ..., x]`, `x == min`
+
+    - `uniform`: Results in `count` uniformly random values between `min` and `max`, generated with `np.random.uniform(min, max, count)`.
+
+        `[x, x, ..., x]`, `x in [min, max)` (includes `low`, but excludes `high`)
+
+    - `range`: `np.arange(min, max, step)`
+
+    - `linspace`: `np.linspace(min, max, count)`
+
+    Specify type of data with `dtype`.
+    """
     def __init__(self, name, min=0, max=1, step=1, count=1, dtype='int', itype='uniform', *args, **kwargs) -> None:
-        if itype == 'uniform':
+        if itype == 'fixed':
+            self._f_gen = None
+        elif itype == 'repeat':
+            self._f_gen = repeat
+        elif itype == 'uniform':
             if dtype == 'int':
                 self._f_gen = partial(np.random.randint, dtype=np.int32)
             elif dtype == 'float':
@@ -53,7 +102,11 @@ class Parameter():
         return f'<Parameter @ 0x{id(self)} {self.name} min={self.min} max={self.max} count={self.count} step={self.step} {self.dtype} {self.itype}>'
     
     def total(self):
-        if self.itype == 'uniform':
+        if self.itype == 'fixed':
+            return 1
+        elif self.itype == 'repeat':
+            return self.count
+        elif self.itype == 'uniform':
             return self.count
         elif self.itype == 'range':
             if self.dtype == 'int':
@@ -64,9 +117,16 @@ class Parameter():
             return self.count
         else:
             raise NotImplementedError
-        
+    
+    def _iter_fixed(self):
+        yield self.min
+
     def reset(self):
-        if self.itype == 'uniform':
+        if self.itype == 'fixed':
+            self._gen = iter((self.min,))
+        elif self.itype == 'repeat':
+            self._gen = iter(self._f_gen(self.min, self.count))
+        elif self.itype == 'uniform':
             self._gen = iter(self._f_gen(self.min, self.max, self.count))
         elif self.itype == 'range':
             self._gen = iter(self._f_gen(self.min, self.max, self.step))
@@ -84,9 +144,11 @@ class Parameter():
 
 class Parameter2D():
     def __init__(self, name, a_x, a_y, b_x, b_y, stops_x, stops_y):
+        """TODO: step size instead of stop count possible as well"""
         self.name = name
         self.limits = ((a_x, a_y), (b_x, b_y))
         self.stops = (stops_x, stops_y)
+        self.itype = '2d'
 
         self.reset()
 
@@ -129,13 +191,17 @@ def pproduct(iters):
                 if key in rest.keys():
                     raise Exception(f"multiple definitions of {key=}")
                 
-                # unroll the thing if it is iterable so it will more easily go into a database
-                try:
-                    for idx, j in enumerate(i):
-                        rest[f'{key}{idx}'] = j
-                except TypeError:
+                # try to unroll the thing if it is iterable so it will more easily go into a database,
+                # unless it is a fixed itype
+                if iters[0].itype != 'fixed':
+                    try:
+                        for idx, j in enumerate(i):
+                            rest[f'{key}{idx}'] = j
+                    except TypeError:
+                        rest[key] = i
+                else:
                     rest[key] = i
-                
+
                 yield rest
 
 def ptotal(iters):
@@ -149,3 +215,36 @@ def pload(fname):
     with open(fname, 'r') as f:
         data = json.load(f)
         return [Parameter(**config) for config in data]
+    
+if __name__ == "__main__":
+    from types import SimpleNamespace
+    from pprint import pprint
+    from tqdm import tqdm
+
+    params = [
+        Parameter('string_const', 'hello world', itype='fixed'),
+        Parameter('array_const', '[1,2,3,4,5]', itype='fixed'), # only using db types makes logging stuff easier
+        Parameter('num_const', 42, itype='fixed'),
+        Parameter('scan', max=5, itype='range'),
+
+        Parameter2D('xy_scanner', a_x=0, a_y=5, b_x=33, b_y=33, stops_x=2, stops_y=2),
+
+        Parameter('glitch_delay', min=400, max=10_000, count=200, itype='uniform'),
+        Parameter('glitch_v', min=0, max=1, count=12, dtype='float', itype='linspace'),
+    ]
+
+    progress = tqdm(enumerate(pproduct(params)), total=ptotal(params), mininterval=1, ncols=80)
+    for idx, settings in progress:
+        try:
+            p = SimpleNamespace(**settings)
+            with progress.external_write_mode():
+                pprint(p)
+        except KeyboardInterrupt:
+            try:
+                x = input('c/q?')
+            except KeyboardInterrupt:
+                break
+            if x == 'c':
+                continue
+            else:
+                break
